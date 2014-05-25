27acbecd9d3f84da81b11b95a26b3909ff251602
tsuttsu305 <tundere@tsuttsu305.net>
2014-05-26 00:28:46 +0900

    BaseUIController Edit

--- src/net/tsuttsu305/tunderemde/BaseUIController.java
+++ src/net/tsuttsu305/tunderemde/BaseUIController.java
@@ -1,5 +1,6 @@
 package net.tsuttsu305.tunderemde;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,23 +14,17 @@ import javafx.scene.control.TextArea;
 import javafx.scene.web.WebView;
 
 public class BaseUIController implements Initializable{
+    @SuppressWarnings("UnusedDeclaration")
+    public static String Charset = "UTF-8";
+
     GithubRawMarkdownRender render;
-    
-	@FXML private TextArea textArea;
-	@FXML private WebView webView;
-	@FXML private Button btn;
-	@FXML private MenuItem close;
-	
-	@FXML
-	public void onPreviewBtn(){
-	    System.out.println("onPreviewBtn");
-	    preview();
-	}
-	
-	@FXML
-	public void onClose(){
-	    System.exit(0);
-	}
+
+    private File nowEditFile = null;
+
+    @FXML private TextArea textArea;
+    @FXML private WebView webView;
+    @FXML private Button btn;
+    @FXML private MenuItem close;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -38,14 +33,58 @@ public class BaseUIController implements Initializable{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        newFile();
     }
-    
-    
-    public void preview(){
+
+    @FXML
+    public void onPreviewBtn(){
+        System.out.println("onPreviewBtn");
+        preview();
+    }
+
+    @FXML
+    public void onClose(){
+        System.exit(0);
+    }
+
+    @FXML
+    public void onOpen(){
+
+    }
+
+    @FXML
+    public void onSave(){
+
+    }
+
+    @FXML
+    public void onNew(){
+
+    }
+
+    private void newFile(){
         try {
-            webView.getEngine().loadContent(render.render(textArea.getText()));
+            File tmp = File.createTempFile("TundereMDE_TMP", ".md.tmp");
+            nowEditFile = tmp;
+
+            textArea.setText("");
+            webView.getEngine().loadContent("");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void preview(){
+        Runnable r = () -> {
+            try {
+                webView.getEngine().loadContent(render.render(textArea.getText()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread t = new Thread(r);
+        t.start();
     }
 }
